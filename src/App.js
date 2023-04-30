a484f6c3fa892fb0356e701c92f34f156ea6bd3f
Kaijun <kaijun_huang@outlook.com>
2023-04-30 18:10:26 +0800

    feat: add scss files and import into components

--- src/App.js
+++ src/App.js
@@ -12,5 +12,4 @@ function App(props) {
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Layout from "./component/layout";
+import Layout from "./component/layout/layout";
 
 import reportWebVitals from "./reportWebVitals";
 
